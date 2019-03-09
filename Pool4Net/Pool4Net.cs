55b9c39f841e19389d9b444aa7db262323d7b299
Farzan Hajian <fh791@yahoo.com>
2019-03-09 17:57:10 +0330

    Initial Development

---
+++ Pool4Net/Pool4Net.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Pool4Net
+{
+    public class Pool4Net<T>
+    {
+        private const string DEFAULT_GROUP = "Default";
+
+        private ConcurrentDictionary<string, ConcurrentBag<T>> storage;
+        private Func<T> generator;
+        private Action<T> cleaner;
+
+        public Pool4Net(Func<T> generator, Action<T> cleaner) : this(generator, cleaner, new[] { DEFAULT_GROUP })
+        {
+        }
+
+        public Pool4Net(Func<T> generator, Action<T> cleaner, IEnumerable<string> groups)
+        {
+            this.generator = generator ?? throw new ArgumentException("The generator cannot be null");
+            this.cleaner = cleaner;
+
+            storage = new ConcurrentDictionary<string, ConcurrentBag<T>>();
+            foreach (string group in groups) storage.GetOrAdd(group, new ConcurrentBag<T>());
+        }
+
+        public T Get()
+        {
+            T result;
+            if (storage[DEFAULT_GROUP].TryTake(out result)) return result;
+            return generator();
+        }
+
+        public T Get(string group)
+        {
+            T result;
+            if (storage[group].TryTake(out result)) return result;
+            return generator();
+        }
+
+        public void Release(T item)
+        {
+            cleaner?.Invoke(item);
+            storage[DEFAULT_GROUP].Add(item);
+        }
+
+        public void Release(string key, T item)
+        {
+            cleaner?.Invoke(item);
+            storage[key].Add(item);
+        }
+    }
+}
---
+++ Pool4NetTest/Program.cs
@@ -0,0 +1,112 @@
+﻿using Pool4Net;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Pool4NetTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            LoadTest();
+            LoadTestDefault();
+            Instantiation();
+
+            Console.ReadLine();
+        }
+
+        private static void LoadTest()
+        {
+            const int TASK_COUNT = 200;
+            const int GROUP_COUNT = 5;
+            const int OBJECT_COUNT_PER_TASK = 1500;
+
+            string[] groups = new string[GROUP_COUNT] { "0", "1", "2", "3", "4" };
+            Pool4Net<TestData> pool = new Pool4Net<TestData>(Generator, null, groups);
+            Task[] tasks = new Task[TASK_COUNT];
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            for (int i = 0; i < TASK_COUNT; i++)
+            {
+                tasks[i] = Task.Run(
+                    () =>
+                    {
+                        string g = (i % GROUP_COUNT).ToString();
+                        for (int j = 0; j < OBJECT_COUNT_PER_TASK; j++)
+                        {
+                            var data = pool.Get(g);
+                            pool.Release(g, data);
+                        }
+                    }
+                );
+            }
+            Task.WaitAll(tasks);
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+
+            TestData Generator()
+            {
+                return new TestData();
+            }
+        }
+
+        private static void LoadTestDefault()
+        {
+            const int TASK_COUNT = 2000;
+            const int OBJECT_COUNT_PER_TASK = 15000;
+
+            Pool4Net<TestData> pool = new Pool4Net<TestData>(Generator, null);
+            Task[] tasks = new Task[TASK_COUNT];
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            for (int i = 0; i < TASK_COUNT; i++)
+            {
+                tasks[i] = Task.Run(
+                    () =>
+                    {
+                        for (int j = 0; j < OBJECT_COUNT_PER_TASK; j++)
+                        {
+                            var data = pool.Get();
+                            pool.Release(data);
+                        }
+                    }
+                );
+            }
+            Task.WaitAll(tasks);
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+
+            TestData Generator()
+            {
+                return new TestData();
+            }
+        }
+
+        private static void Instantiation()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            TestData[] data = new TestData[300000];
+
+            for (int i = 0; i < 300000; i++)
+            {
+                data[i] = new TestData();
+            }
+
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+        }
+
+        private class TestData
+        {
+            public int Value1 { get; set; }
+            public string Value2 { get; set; }
+            public decimal Value3 { get; set; }
+        }
+    }
+}
